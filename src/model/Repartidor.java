45400c522cf26802d0f39e9b04fef847d8e95dd5
strakelov <gabogomezpizarro@gmail.com>
2026-09-03 10:48:43 -0400

    S4: Concurrencia en sistema de pedidos - Corrección

--- src/model/Repartidor.java
+++ src/model/Repartidor.java
@@ -20,9 +20,9 @@ public class Repartidor implements Runnable {
             try {
                 Thread.sleep(random.nextInt(10) * 2000);
                 System.out.println("Pedido encargado. Su id es: #" + pedido.getIdPedido());
-                Thread.sleep(random.nextInt(10) * 2000);
+                Thread.sleep(random.nextInt(7) * 2000);
                 System.out.println("[Repartidor: " + nombre + "] " + pedido.getTipoPedido() + " #" + pedido.getIdPedido() + " en camino.");
-                Thread.sleep(random.nextInt(10) * 2000);
+                Thread.sleep(random.nextInt(5) * 2000);
                 System.out.println("Pedido #" + pedido.getIdPedido() + " entregado correctamente.");
             } catch (InterruptedException e) {
                 System.out.println("Entrega interrumpida del pedido #" + pedido.getIdPedido());
